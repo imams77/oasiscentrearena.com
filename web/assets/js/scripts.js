349f7beb8cb11cc63276c10470b38e228c92b126
Imam Stevanno <im.stevanno@gmail.com>
2018-04-12 10:46:19 +0700

    add facility, new home

--- web/assets/js/scripts.js
+++ web/assets/js/scripts.js
@@ -16,7 +16,7 @@ function clickAnywhereToHide(selectedClass, removedClass, reverse = false) {
   })
   
 }
-function expandSubmenu(submenu) {
+function expandSubmenu (submenu) {
   // $('.has-submenu').addClass('collapsed')
   var hasSubMenu = document.querySelectorAll('.oca-menu > ul > li.has-submenu')
   for(let i = 0; i < hasSubMenu.length; i++) {
@@ -27,11 +27,28 @@ function expandSubmenu(submenu) {
     }
   }
 }
-function calendarTriangle() {
+function calendarTriangle () {
   let x = document.querySelectorAll('.calendar-container')
   for (let i = 0; i < x.length; i++) {
     x[i].style.zIndex = i
   }
+}
+function setHeroVideoTextPosition (windowHeight) {
+  let homeContainer = document.getElementById('home-video')
+  let containerHeight = ''
+  for (let i = 0; i < homeContainer.children.length; i++) {
+    console.log(homeContainer.children[i])
+    if (homeContainer.children[i].classList[0] === 'text') {
+      containerHeight = homeContainer.children[i].clientHeight 
+      console.log(containerHeight, windowHeight)
+      homeContainer.children[i].style.top = (windowHeight / 2) - (containerHeight / 2) + 'px'
+    }
+  }
+}
+function bodyWrapperParallax () {
+  let wrapper = document.querySelectorAll('.wrapper')[0]
+  let scrollValue = window.scrollY
+  wrapper.style.backgroundPositionY = -scrollValue/15 + 'px'
 }
 // get staired padding like
 // lorem
@@ -55,16 +72,21 @@ function animate() {
       x[i].classList.remove('active')
     }
   }
-  let homeCalendar = document.getElementById('home-calendar')
+  let homeCalendar = document.getElementById('footer-calendar')
   if(scrollValue >= homeCalendar.offsetTop - windowHeight / 1.5) {
     homeCalendar.classList.add('active')
   } else {
     homeCalendar.classList.remove('active')
   }
 }
-var calendarBackground = document.getElementById('home-calendar')
+var calendarBackground = document.getElementById('footer-calendar')
 var windowHeight = window.innerHeight
 $(document).ready(() => {
+  $('.slick').slick({
+    arrows: false
+  })
+  setHeroVideoTextPosition(windowHeight)
+  $('.hero-slider').slick()
   clickAnywhereToHide('.has-submenu','collapsed', true)
   //add panah ke bawah klo menu nya punya submenu
   $('.has-submenu > a').append('&nbsp; &nbsp;<i class="fas fa-chevron-down"></i>')
@@ -75,5 +97,17 @@ $(document).ready(() => {
 
 $(document).scroll(() => {
   animate()
+  bodyWrapperParallax()
+  if ($(window).scrollTop() >= windowHeight) {
+    $('header.fixed').addClass('active')
+  } else {
+    $('header.fixed').removeClass('active')
+  }
+  
   // calendarBackground.style.backgroundPositionY = window.scrollY / 3 + 'px'
+})
+
+$(window).resize(() => {
+  windowHeight = window.innerHeight
+  setHeroVideoTextPosition(windowHeight)
 })
